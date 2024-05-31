7fc806b32c39af4689ff857e1067aab3908efe8e
t-nihikoori <t_nishikoori@yahoo.co.jp>
2024-05-31 10:54:43 +0900

    lesson_python.py

---
+++ kadai_008/lesson_python-3.py
@@ -0,0 +1,26 @@
+import random
+var = random.randint(0,100)
+print(var)
+
+if var % 3 == 0 and var % 5 == 0:
+  print("FizzBuzz")
+  
+elif var % 3 == 0:
+  print("Fizz")
+
+elif var % 5 == 0:
+  print("Buzz")
+
+else:
+  print(var)
+  
+  
+  
+
+
+
+
+
+
+  
+  
