121e0c6c20088d32b6a76b5b24cbe82a45472bea
t-nihikoori <t_nishikoori@yahoo.co.jp>
2024-06-01 18:02:49 +0900

    lesson_python-4.py

---
+++ kadai_011/lesson_python-4.py
@@ -0,0 +1,23 @@
+array = ["水","金","地","火","木","土","天","海","冥"]
+
+#for文
+for value in array:
+  print(value)
+  
+
+#while文
+i = 0
+
+while i < len(array):
+  print(array[i])
+  i += 1
+
+  
+ 
+
+
+   
+  
+  
+
+
